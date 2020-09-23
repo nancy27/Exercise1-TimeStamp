10a112faffc506deed8fe59519dae44929f472dc
nancy27 <54560171+nancy27@users.noreply.github.com>
2020-09-23 12:38:45 -0500

    added changes

--- exercise1/server.js
+++ exercise1/server.js
@@ -2,8 +2,31 @@ var express= require('express');
 var timestampRoutes= require('./routes/timestampRoutes')
 var app= express();
 
-app.use('/api/timestamp',timestampRoutes);
+app.get("/api/timestamp/", (req, res) => {
+    res.json({ unix: Date.now(), utc: Date() });
+  });
+  
+  app.get("/api/timestamp/:date_string", (req, res) => {
+    let dateString = req.params.date_string;
+  
+    //A 4 digit number is a valid ISO-8601 for the beginning of that year
+    //5 digits or more must be a unix time, until we reach a year 10,000 problem
+    if (/\d{5,}/.test(dateString)) {
+      dateInt = parseInt(dateString);
+      //Date regards numbers as unix timestamps, strings are processed differently
+      res.json({ unix: dateString, utc: new Date(dateInt).toUTCString() });
+    }
+  
+    let dateObject = new Date(dateString);
+  
+    if (dateObject.toString() === "Invalid Date") {
+      res.json({ error: "Invalid Date" });
+    } else {
+      res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+    }
+  });
+
+
 app.listen(9000, () => {
     console.log('Server started')
 })
-
